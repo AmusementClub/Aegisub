7e1cc6382a1f0a2ca97a6316fda7d5ef544d32a6
akarin <i@akarin.info>
2023-02-21 14:23:45 +0900

    Revert "fix build for latest boost, skip test on windows"
    This reverts commit 49e383f5243c30d3cdaaea8911b487844b9fcd50.
    
    Signed-off-by: akarin <i@akarin.info>

--- libaegisub/include/libaegisub/lua/utils.h
+++ libaegisub/include/libaegisub/lua/utils.h
@@ -22,7 +22,6 @@
 #include <type_traits>
 
 #include <boost/config.hpp>
-#include <boost/flyweight.hpp>
 
 #ifndef BOOST_NORETURN
 #include <boost/exception/detail/attribute_noreturn.hpp>
@@ -76,12 +75,6 @@ void push_value(lua_State *L, std::vector<T> const& value) {
 		push_value(L, value[i]);
 		lua_rawseti(L, -2, i + 1);
 	}
-}
-
-template<typename T>
-inline void push_value(lua_State *L, boost::flyweight<T> const& value) {
-	const T& v = value;
-	push_value(L, v);
 }
 
 int exception_wrapper(lua_State *L, int (*func)(lua_State *L));

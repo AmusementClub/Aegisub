cf526385e4f6114ae882e6d55335cdb365a7ceae
MIRIMIRIM <59959583+MIRIMIRIM@users.noreply.github.com>
2026-03-18 01:01:58 +0800

    require external AviSynth headers when AviSynth is enabled

--- cmake/FindAviSynth.cmake
+++ cmake/FindAviSynth.cmake
@@ -1,31 +1,24 @@
 find_package(PkgConfig QUIET)
 pkg_check_modules(PC_AviSynth QUIET avisynth)
-find_path(AviSynth_INCLUDE_DIRS
+if(DEFINED AviSynth_INCLUDE_DIR AND NOT IS_ABSOLUTE "${AviSynth_INCLUDE_DIR}")
+  get_filename_component(AviSynth_INCLUDE_DIR "${AviSynth_INCLUDE_DIR}" ABSOLUTE BASE_DIR "${CMAKE_SOURCE_DIR}")
+endif()
+if(AviSynth_INCLUDE_DIR AND NOT EXISTS "${AviSynth_INCLUDE_DIR}/avisynth.h")
+  unset(AviSynth_INCLUDE_DIR CACHE)
+  unset(AviSynth_INCLUDE_DIR)
+endif()
+find_path(AviSynth_INCLUDE_DIR
   NAMES avisynth.h
   PATHS "C:/Program Files/AviSynth+/FilterSDK/include" "C:/Program Files (x86)/AviSynth+/FilterSDK/include"
   PATH_SUFFIXES avisynth
+  HINTS ${AviSynth_INCLUDE_DIR}
   HINTS ${PC_AviSynth_INCLUDE_DIRS}
 )
-if(WIN32)
-  find_file(AviSynth_SHARED_LIBRARY
-    NAMES AviSynth.dll
-    PATHS "C:/Windows/System32/"
-    PATH_SUFFIXES bin c_api
-    HINTS ${PC_AviSynth_LIBRARY_DIRS}
-  )
-else()
-  find_library(AviSynth_SHARED_LIBRARY
-    NAMES avisynth
-    PATH_SUFFIXES c_api
-    HINTS ${PC_AviSynth_LIBRARY_DIRS}
-  )
-endif()
 set(AviSynth_VERSION ${PC_AviSynth_VERSION})
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(AviSynth
   FOUND_VAR AviSynth_FOUND
   REQUIRED_VARS
-    AviSynth_SHARED_LIBRARY
-    AviSynth_INCLUDE_DIRS
+    AviSynth_INCLUDE_DIR
   VERSION_VAR AviSynth_VERSION
 )

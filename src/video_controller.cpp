AmusementClub/Aegisub

commit 19e8f19e521234d0da6dd0906a1e07bd9726f747

Redesign project file handling
Add a new Project class which is responsible for everything related to
opening and closing audio, video, subtitles, timecodes and keyframes.
This pulls almost everything not directly related to playing audio/video
out of the audio and video controllers, pulls more crap out of
FrameMain, and happens to make things a little simpler in the process.

src/video_controller.cpp
@@ -1,0 +1,274 @@
+// Copyright (c) 2005-2007, Rodrigo Braz Monteiro
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//   * Redistributions of source code must retain the above copyright notice,
+//     this list of conditions and the following disclaimer.
+//   * Redistributions in binary form must reproduce the above copyright notice,
+//     this list of conditions and the following disclaimer in the documentation
+//     and/or other materials provided with the distribution.
+//   * Neither the name of the Aegisub Group nor the names of its contributors
+//     may be used to endorse or promote products derived from this software
+//     without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+// Aegisub Project http://www.aegisub.org/
+
+#include "video_controller.h"
+
+#include "ass_dialogue.h"
+#include "ass_file.h"
+#include "ass_time.h"
+#include "audio_controller.h"
+#include "compat.h"
+#include "dialog_progress.h"
+#include "dialog_video_properties.h"
+#include "include/aegisub/context.h"
+#include "include/aegisub/video_provider.h"
+#include "mkv_wrap.h"
+#include "options.h"
+#include "project.h"
+#include "selection_controller.h"
+#include "subs_controller.h"
+#include "time_range.h"
+#include "async_video_provider.h"
+#include "utils.h"
+#include "video_frame.h"
+
+#include <libaegisub/fs.h>
+#include <libaegisub/path.h>
+#include <libaegisub/make_unique.h>
+
+#include <wx/msgdlg.h>
+
+VideoController::VideoController(agi::Context *c)
+: context(c)
+, playAudioOnStep(OPT_GET("Audio/Plays When Stepping Video"))
+, connections(agi::signal::make_vector({
+	context->ass->AddCommitListener(&VideoController::OnSubtitlesCommit, this),
+	context->project->AddVideoProviderListener(&VideoController::OnNewVideoProvider, this),
+	context->selectionController->AddActiveLineListener(&VideoController::OnActiveLineChanged, this),
+	context->subsController->AddFileSaveListener(&VideoController::OnSubtitlesSave, this),
+}))
+{
+	Bind(EVT_VIDEO_ERROR, &VideoController::OnVideoError, this);
+	Bind(EVT_SUBTITLES_ERROR, &VideoController::OnSubtitlesError, this);
+	playback.Bind(wxEVT_TIMER, &VideoController::OnPlayTimer, this);
+}
+
+void VideoController::OnNewVideoProvider(AsyncVideoProvider *new_provider) {
+	Stop();
+	frame_n = 0;
+
+	provider = new_provider;
+	if (!provider) {
+		color_matrix.clear();
+		return;
+	}
+
+	color_matrix = provider->GetColorSpace();
+	double dar = provider->GetDAR();
+	if (dar > 0)
+		SetAspectRatio(dar);
+
+	JumpToFrame(0);
+}
+
+void VideoController::OnSubtitlesCommit(int type, std::set<const AssDialogue *> const& changed) {
+	if (!provider) return;
+
+	if ((type & AssFile::COMMIT_SCRIPTINFO) || type == AssFile::COMMIT_NEW) {
+		auto new_matrix = context->ass->GetScriptInfo("YCbCr Matrix");
+		if (!new_matrix.empty() && new_matrix != color_matrix) {
+			color_matrix = new_matrix;
+			provider->SetColorSpace(new_matrix);
+		}
+	}
+
+	if (changed.empty() || no_amend)
+		provider->LoadSubtitles(context->ass.get());
+	else
+		provider->UpdateSubtitles(context->ass.get(), changed);
+	if (!IsPlaying())
+		provider->GetFrame(frame_n, TimeAtFrame(frame_n));
+
+	no_amend = false;
+}
+
+void VideoController::OnSubtitlesSave() {
+	no_amend = true;
+
+	if (!provider) {
+		context->ass->SaveUIState("Video Aspect Ratio", "");
+		context->ass->SaveUIState("Video Position", "");
+		return;
+	}
+
+	std::string ar;
+	if (ar_type == AspectRatio::Custom)
+		ar = "c" + std::to_string(ar_value);
+	else
+		ar = std::to_string((int)ar_type);
+
+	context->ass->SaveUIState("Video Aspect Ratio", ar);
+	context->ass->SaveUIState("Video Position", std::to_string(frame_n));
+}
+
+void VideoController::OnActiveLineChanged(AssDialogue *line) {
+	if (line && provider && OPT_GET("Video/Subtitle Sync")->GetBool())
+		JumpToTime(line->Start);
+}
+
+void VideoController::RequestFrame() {
+	provider->RequestFrame(frame_n, TimeAtFrame(frame_n));
+}
+
+void VideoController::JumpToFrame(int n) {
+	if (!provider) return;
+
+	bool was_playing = IsPlaying();
+	if (was_playing)
+		Stop();
+
+	frame_n = mid(0, n, provider->GetFrameCount() - 1);
+	RequestFrame();
+	Seek(frame_n);
+
+	if (was_playing)
+		Play();
+}
+
+void VideoController::JumpToTime(int ms, agi::vfr::Time end) {
+	JumpToFrame(FrameAtTime(ms, end));
+}
+
+void VideoController::NextFrame() {
+	if (!provider || IsPlaying() || frame_n == provider->GetFrameCount())
+		return;
+
+	JumpToFrame(frame_n + 1);
+	if (playAudioOnStep->GetBool())
+		context->audioController->PlayRange(TimeRange(TimeAtFrame(frame_n - 1), TimeAtFrame(frame_n)));
+}
+
+void VideoController::PrevFrame() {
+	if (!provider || IsPlaying() || frame_n == 0)
+		return;
+
+	JumpToFrame(frame_n - 1);
+	if (playAudioOnStep->GetBool())
+		context->audioController->PlayRange(TimeRange(TimeAtFrame(frame_n), TimeAtFrame(frame_n + 1)));
+}
+
+void VideoController::Play() {
+	if (IsPlaying()) {
+		Stop();
+		return;
+	}
+
+	if (!provider) return;
+
+	start_ms = TimeAtFrame(frame_n);
+	end_frame = provider->GetFrameCount() - 1;
+
+	context->audioController->PlayToEnd(start_ms);
+
+	playback_start_time = std::chrono::steady_clock::now();
+	playback.Start(10);
+}
+
+void VideoController::PlayLine() {
+	Stop();
+
+	AssDialogue *curline = context->selectionController->GetActiveLine();
+	if (!curline) return;
+
+	context->audioController->PlayRange(TimeRange(curline->Start, curline->End));
+
+	// Round-trip conversion to convert start to exact
+	int startFrame = FrameAtTime(context->selectionController->GetActiveLine()->Start, agi::vfr::START);
+	start_ms = TimeAtFrame(startFrame);
+	end_frame = FrameAtTime(context->selectionController->GetActiveLine()->End, agi::vfr::END) + 1;
+
+	JumpToFrame(startFrame);
+
+	playback_start_time = std::chrono::steady_clock::now();
+	playback.Start(10);
+}
+
+void VideoController::Stop() {
+	if (IsPlaying()) {
+		playback.Stop();
+		context->audioController->Stop();
+	}
+}
+
+void VideoController::OnPlayTimer(wxTimerEvent &) {
+	using namespace std::chrono;
+	int next_frame = FrameAtTime(start_ms + duration_cast<milliseconds>(steady_clock::now() - playback_start_time).count());
+	if (next_frame == frame_n) return;
+
+	if (next_frame >= end_frame)
+		Stop();
+	else {
+		frame_n = next_frame;
+		RequestFrame();
+		Seek(frame_n);
+	}
+}
+
+double VideoController::GetARFromType(AspectRatio type) const {
+	switch (type) {
+		case AspectRatio::Default:    return (double)provider->GetWidth()/provider->GetHeight();
+		case AspectRatio::Fullscreen: return 4.0/3.0;
+		case AspectRatio::Widescreen: return 16.0/9.0;
+		case AspectRatio::Cinematic:  return 2.35;
+	}
+	throw agi::InternalError("Bad AR type", nullptr);
+}
+
+void VideoController::SetAspectRatio(double value) {
+	ar_type = AspectRatio::Custom;
+	ar_value = mid(.5, value, 5.);
+	ARChange(ar_type, ar_value);
+}
+
+void VideoController::SetAspectRatio(AspectRatio type) {
+	ar_value = mid(.5, GetARFromType(type), 5.);
+	ar_type = type;
+	ARChange(ar_type, ar_value);
+}
+
+int VideoController::TimeAtFrame(int frame, agi::vfr::Time type) const {
+	return context->project->Timecodes().TimeAtFrame(frame, type);
+}
+
+int VideoController::FrameAtTime(int time, agi::vfr::Time type) const {
+	return context->project->Timecodes().FrameAtTime(time, type);
+}
+
+void VideoController::OnVideoError(VideoProviderErrorEvent const& err) {
+	wxLogError(
+		"Failed seeking video. The video file may be corrupt or incomplete.\n"
+		"Error message reported: %s",
+		to_wx(err.GetMessage()));
+}
+
+void VideoController::OnSubtitlesError(SubtitlesProviderErrorEvent const& err) {
+	wxLogError(
+		"Failed rendering subtitles. Error message reported: %s",
+		to_wx(err.GetMessage()));
+}

src/video_controller.h
@@ -27,28 +27,17 @@
 //
 // Aegisub Project http://www.aegisub.org/
 
-/// @file video_context.h
-/// @see video_context.cpp
-/// @ingroup video
-///
-
-#include <libaegisub/fs_fwd.h>
 #include <libaegisub/signal.h>
 #include <libaegisub/vfr.h>
 
-#include <boost/filesystem/path.hpp>
 #include <chrono>
-#include <ctime>
-#include <list>
 #include <memory>
 #include <set>
 
 #include <wx/timer.h>
 
 class AssDialogue;
-class DialogProgress;
-class ThreadedFrameSource;
-class VideoProvider;
+class AsyncVideoProvider;
 struct SubtitlesProviderErrorEvent;
 struct VideoFrame;
 struct VideoProviderErrorEvent;
@@ -66,46 +55,21 @@ enum class AspectRatio {
 	Custom
 };
 
-/// @class VideoContext
-/// @brief Manage a bunch of things vaguely related to video playback
-///
-/// VideoContext's core responsibility is opening and playing videos. Along
-/// with that, it also manages video timecodes and keyframes, and some
-/// video-related UI properties
-class VideoContext final : public wxEvtHandler {
+/// Manage stuff related to video playback
+class VideoController final : public wxEvtHandler {
 	/// Current frame number changed (new frame number)
 	agi::signal::Signal<int> Seek;
-	/// A new video was opened
-	agi::signal::Signal<> VideoOpen;
-	/// New keyframes opened (new keyframe data)
-	agi::signal::Signal<std::vector<int> const&> KeyframesOpen;
-	/// New timecodes opened (new timecode data)
-	agi::signal::Signal<agi::vfr::Framerate const&> TimecodesOpen;
 	/// Aspect ratio was changed (type, value)
 	agi::signal::Signal<AspectRatio, double> ARChange;
 
 	agi::Context *context;
 
-	DialogProgress *progress = nullptr;
-
 	/// The video provider owned by the threaded frame source, or nullptr if no
 	/// video is open
-	VideoProvider *video_provider = nullptr;
-
-	/// Asynchronous provider of video frames
-	std::unique_ptr<ThreadedFrameSource> provider;
-
-	/// Filename of currently open video
-	agi::fs::path video_filename;
+	AsyncVideoProvider *provider = nullptr;
 
 	/// Last seen script color matrix
 	std::string color_matrix;
-
-	/// List of frame numbers which are keyframes
-	std::vector<int> keyframes;
-
-	/// File name of the currently open keyframes or empty if keyframes are not overridden
-	agi::fs::path keyframes_filename;
 
 	/// Playback timer used to periodically check if we should go to the next
 	/// frame while playing video
@@ -132,15 +96,10 @@ class VideoContext final : public wxEvtHandler {
 	/// The current AR type
 	AspectRatio ar_type = AspectRatio::Default;
 
-	/// Does the currently loaded video file have subtitles muxed into it?
-	bool has_subtitles = false;
-
-	/// Filename of the currently loaded timecodes file, or empty if timecodes
-	/// have not been overridden
-	agi::fs::path timecodes_filename;
-
 	/// Cached option for audio playing when frame stepping
 	const agi::OptionValue* playAudioOnStep;
+
+	std::vector<agi::signal::Connection> connections;
 
 	/// Amending the frame source's copy of the subtitle file requires that it
 	/// be kept in perfect sync. Saving the file can add lines to the file
@@ -150,57 +109,21 @@ class VideoContext final : public wxEvtHandler {
 
 	void OnPlayTimer(wxTimerEvent &event);
 
-	/// The timecodes from the video file
-	agi::vfr::Framerate video_fps;
-	/// External timecode which have been loaded, if any
-	agi::vfr::Framerate ovr_fps;
-
 	void OnVideoError(VideoProviderErrorEvent const& err);
 	void OnSubtitlesError(SubtitlesProviderErrorEvent const& err);
 
 	void OnSubtitlesCommit(int type, std::set<const AssDialogue *> const& changed);
 	void OnSubtitlesSave();
+	void OnNewVideoProvider(AsyncVideoProvider *provider);
+	void OnActiveLineChanged(AssDialogue *line);
 
-	/// Close the video, keyframes and timecodes
-	void Reset();
+	void RequestFrame();
 
 public:
-	VideoContext(agi::Context *context);
-	~VideoContext();
-
-	/// @brief Get the video provider used for the currently open video
-	VideoProvider *GetProvider() const { return video_provider; }
-
-	/// Synchronously get a video frame
-	/// @param n Frame number to get
-	/// @param raw If true, subtitles are not rendered on the frame
-	/// @return The requested frame
-	std::shared_ptr<VideoFrame> GetFrame(int n, bool raw = false);
-
-	/// Asynchronously get a video frame, triggering a EVT_FRAME_READY event when it's ready
-	/// @param n Frame number to get
-	void GetFrameAsync(int n);
-
-	/// Is there a video loaded?
-	bool IsLoaded() const { return !!video_provider; }
-
-	/// Get the file name of the currently open video, if any
-	agi::fs::path GetVideoName() const { return video_filename; }
+	VideoController(agi::Context *context);
 
 	/// Is the video currently playing?
 	bool IsPlaying() const { return playback.IsRunning(); }
-
-	/// Does the video file loaded have muxed subtitles that we can load?
-	bool HasSubtitles() const { return has_subtitles; }
-
-	/// Get the width of the currently open video
-	int GetWidth() const;
-
-	/// Get the height of the currently open video
-	int GetHeight() const;
-
-	/// Get the length in frames of the currently open video
-	int GetLength() const;
 
 	/// Get the current frame number
 	int GetFrameN() const { return frame_n; }
@@ -220,12 +143,6 @@ public:
 
 	/// Get the current aspect ratio of the video
 	double GetAspectRatioValue() const { return ar_value; }
-
-	/// @brief Open a new video
-	/// @param filename Video to open, or empty to close the current video
-	void SetVideo(const agi::fs::path &filename);
-	/// @brief Close and reopen the current video
-	void Reload();
 
 	/// @brief Jump to the beginning of a frame
 	/// @param n Frame number to jump to
@@ -247,28 +164,7 @@ public:
 	void Stop();
 
 	DEFINE_SIGNAL_ADDERS(Seek, AddSeekListener)
-	DEFINE_SIGNAL_ADDERS(VideoOpen, AddVideoOpenListener)
-	DEFINE_SIGNAL_ADDERS(KeyframesOpen, AddKeyframesListener)
-	DEFINE_SIGNAL_ADDERS(TimecodesOpen, AddTimecodesListener)
 	DEFINE_SIGNAL_ADDERS(ARChange, AddARChangeListener)
-
-	const std::vector<int>& GetKeyFrames() const { return keyframes; };
-	agi::fs::path GetKeyFramesName() const { return keyframes_filename; }
-	void LoadKeyframes(agi::fs::path const& filename);
-	void SaveKeyframes(agi::fs::path const& filename);
-	void CloseKeyframes();
-	bool OverKeyFramesLoaded() const { return !keyframes_filename.empty(); }
-	bool KeyFramesLoaded() const { return !keyframes.empty(); }
-
-	agi::fs::path GetTimecodesName() const { return timecodes_filename; }
-	void LoadTimecodes(agi::fs::path const& filename);
-	void SaveTimecodes(agi::fs::path const& filename);
-	void CloseTimecodes();
-	bool OverTimecodesLoaded() const { return ovr_fps.IsLoaded(); }
-	bool TimecodesLoaded() const { return video_fps.IsLoaded() || ovr_fps.IsLoaded(); };
-
-	const agi::vfr::Framerate& FPS() const { return ovr_fps.IsLoaded() ? ovr_fps : video_fps; }
-	const agi::vfr::Framerate& VideoFPS() const { return video_fps; }
 
 	int TimeAtFrame(int frame, agi::vfr::Time type = agi::vfr::EXACT) const;
 	int FrameAtTime(int time, agi::vfr::Time type = agi::vfr::EXACT) const;

commit 727c2b9a2322e7807c159efaf7728fe2475890dd

Fix video playback final frame boundary
Use an exclusive playback end frame for Play(), matching the existing PlayLine() one-past-end semantics. When the playback clock reaches or crosses the stop boundary, clamp to the last valid frame before stopping so the final frame is actually displayed.

Also update the end_frame comment to reflect that it stores a one-past-end boundary rather than the last valid frame index.

Original bug introduced in 19e8f19e52 ("Redesign project file handling", 2014-05-21). That commit paired end_frame = provider->GetFrameCount() - 1 with a >= stop check in OnPlayTimer(), which caused playback to stop before presenting the last frame.

src/video_controller.cpp
@@ -197,7 +197,7 @@ bool VideoController::PreparePlayback(PlaybackMode mode, int start_frame, int ra
 		core.audioController->PlayRange(TimeRange(start_ms, playback_end_ms));
 	}
 	else {
-		end_frame = provider->GetFrameCount() - 1;
+		end_frame = provider->GetFrameCount();
 		core.audioController->PlayToEnd(start_ms);
 	}
 	playback_uses_audio_authority = core.audioController->IsPlaying();
@@ -274,15 +274,19 @@ void VideoController::OnPlayTimer() {
 
 	int next_frame = FrameAtTime(authority_time_ms);
 	perf_trace::ObserveVideoPlaybackTick(next_frame);
-	if (next_frame == frame_n) return;
 
-	if (next_frame >= end_frame)
-		Stop();
-	else {
+	bool const reached_end = next_frame >= end_frame;
+	if (reached_end)
+		next_frame = end_frame - 1;
+
+	if (next_frame != frame_n) {
 		frame_n = next_frame;
 		RequestFrame();
 		PlaybackFrameAdvanced(frame_n);
 	}
+
+	if (reached_end)
+		Stop();
 }
 
 double VideoController::GetARFromType(AspectRatio type) const {

src/video_controller.h
@@ -91,7 +91,7 @@ class VideoController final {
 	/// video is not currently playing
 	int start_ms = 0;
 
-	/// The last frame to play if video is currently playing
+	/// One past the last frame to play if video is currently playing
 	int end_frame = 0;
 	enum class PlaybackMode {
 		None,
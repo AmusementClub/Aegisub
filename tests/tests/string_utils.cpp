eb9354984e09904b5e7a52d58ba4d46b54062abd
MIRIMIRIM <59959583+MIRIMIRIM@users.noreply.github.com>
2026-06-14 14:46:38 +0800

    find: fix test
    0cb37573806ef22ccdf10b72835c9ea964043046

--- tests/tests/string_utils.cpp
+++ tests/tests/string_utils.cpp
@@ -1,17 +1,3 @@
-// Copyright (c) 2026
-//
-// Permission to use, copy, modify, and distribute this software for any
-// purpose with or without fee is hereby granted, provided that the above
-// copyright notice and this permission notice appear in all copies.
-//
-// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-// WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-// MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-// ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-// WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-// ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-// OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-
 #include <libaegisub/string_utils.h>
 
 #include <main.h>
@@ -123,6 +109,6 @@ TEST(lagi_string_utils, utf8_icase_searcher_is_case_insensitive_ascii) {
 
 	expect_same_match(utf8_find_icase("Hello Foo Bar", searcher), sized_match{6, 3});
 	expect_same_match(utf8_find_icase("FOO at start", searcher), sized_match{0, 3});
-	expect_same_match(utf8_find_icase("ends with foo", searcher), sized_match{9, 3});
+	expect_same_match(utf8_find_icase("ends with foo", searcher), sized_match{10, 3});
 	expect_same_match(utf8_find_icase("no match here", searcher), sized_match{});
 }

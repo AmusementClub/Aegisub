b4fbbb84f9472dbb6ffed58ba24ff10011af96b0
MIRIMIRIM <59959583+MIRIMIRIM@users.noreply.github.com>
2026-08-01 06:19:25 +0800

    build: set WITH_SKIA_AUDIO_DISPLAY=ON

--- build/build-win-vcpkg.ps1
+++ build/build-win-vcpkg.ps1
@@ -117,6 +117,7 @@ function Configure-CMake([string]$CMakePath, [string]$GeneratorName) {
         "-DWITH_PFFFT=ON",
         '-DAEGISUB_MATROSKA_PARSING=ON',
         "-DWITH_DRAWING_SKIA=ON",
+        "-DWITH_SKIA_AUDIO_DISPLAY=ON",
         "-DZ_VCPKG_POWERSHELL_PATH:FILEPATH=$resolvedPowerShellExecutable",
         "-DCMAKE_CXX_FLAGS=$CMakeCxxFlags",
         "-DCMAKE_C_FLAGS=$CMakeCFlags",
